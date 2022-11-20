cb0aa20103af21ffd8139330b8bd97d23bd41a12
shahfaisal12 <faisalmkd02@gmail.com>
2022-11-20 12:17:44 +0500

    'footer'

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,102 +2,104 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   <div className="footer-section">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-2">
-          <h6>App</h6>
-          <ul>
-            <li>Buy and Sell</li>
-            <li>Crypto Earn</li>
-            <li>Crypto Credit</li>
-            <li>Crypto.com Pay</li>
-          </ul>
-        </div>
-        <div className="col-md-2">
-          <h6>Exchange</h6>
-          <ul>
-            <li>Exchange Home</li>
-            <li>Margin Trading</li>
-            <li>Derivatives Trading</li>
-            <li>The Syndicate</li>
-            <li>Supercharger</li>
-            <li>Trading Arena</li>
-            <li>Commerce</li>
-          </ul>
+    <div className="footer-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <ul>
+              <h6>App</h6>
+              <li>Buy and Sell</li>
+              <li>Crypto Earn</li>
+              <li>Crypto Credit</li>
+              <li>Crypto.com Pay</li>
+            </ul>
           </div>
-        <div className="col-md-2">
-          <h6>Blockchain</h6>
-          <ul>
-          <li></li>
-            <li>Crypto.org Chain</li>
-            <li>Chain Explorer</li>
-          </ul>
+          <div className="col-md-2">
+            <ul>
+              <h6>Exchange</h6>
+              <li>Exchange Home</li>
+              <li>Margin Trading</li>
+              <li>Derivatives Trading</li>
+              <li>The Syndicate</li>
+              <li>Supercharger</li>
+              <li>Trading Arena</li>
+              <li>Commerce</li>
+            </ul>
           </div>
-        <div className="col-md-2">
-        <h6>Resources</h6>
-          <ul>
-          <li>Prices</li>
-            <li>Site Widgets</li>
-            <li>Tax</li>
-            <li>Support</li>
-          </ul>
-        </div>
-        <div className="col-md-4">
-        <div className="btn-icon">
-          <img src="assets/appstorefooter.svg" alt="" />
-          <img src="assets/googlefooter.svg" alt="" />
-        </div>
+          <div className="col-md-2">
+            <ul>
+              <h6>Blockchain</h6>
+              <li></li>
+              <li>Crypto.org Chain</li>
+              <li>Chain Explorer</li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <ul>
+              <h6>Resources</h6>
+              <li>Prices</li>
+              <li>Site Widgets</li>
+              <li>Tax</li>
+              <li>Support</li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <div className="btn-icon">
+              <img src="assets/appstorefooter.svg" alt="" />
+              <img src="assets/googlefooter.svg" alt="" />
 
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-2">
-          <h6>Cards</h6>
-          <ul>
-            <li>Metal Visa Cards</li>
-          </ul>
-        </div>
-        <div className="col-md-2">
-          <h6>Commerce</h6>
-          <ul>
-            <li>Pay for Business</li>
-          </ul>
+            </div>
+
           </div>
-        <div className="col-md-2">
-          <h6>DeFi</h6>
-          <ul>
-          <li></li>
-            <li>Wallet</li>
-            <li>Swap</li>
-            <li>Earn</li>
-            <li>Dashboard</li>
-          </ul>
+        </div>
+        <div className="row mt-30">
+          <div className="col-md-2">
+            <ul>
+              <h6>Cards</h6>
+              <li>Metal Visa Cards</li>
+            </ul>
           </div>
-        <div className="col-md-2">
-        <h6>Company</h6>
-          <ul>
-          <li>About</li>
-            <li>Careers</li>
-            <li>News</li>
-            <li>Security</li>
-            <li>Contact</li>
-          </ul>
+          <div className="col-md-2">
+            <ul>
+              <h6>Commerce</h6>
+              <li>Pay for Business</li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <ul>
+              <h6>DeFi</h6>
+              <li>Wallet</li>
+              <li>Swap</li>
+              <li>Earn</li>
+              <li>Dashboard</li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <ul>
+              <h6>Company</h6>
+              <li>About</li>
+              <li>Careers</li>
+              <li>News</li>
+              <li>Security</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <ul>
+              <h6>Learn</h6>
+              <li>What's Trending</li>
+              <li>Product News</li>
+              <li>Events</li>
+              <li>University</li>
+              <li>Research</li>
+              <li>Market Updates</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-md-4">
-        <h6>Learn</h6>
-          <ul>
-          <li>What's Trending</li>
-            <li>Product News</li>
-            <li>Events</li>
-            <li>University</li>
-            <li>Research</li>
-            <li>Market Updates</li>
-          </ul>
-        </div>
+        <div className="line"></div>
+        <div className="copy-right">Copyright © 2018 - 2022 Crypto.com. All rights reserved.</div>
       </div>
     </div>
-   </div>
   )
 }
 
